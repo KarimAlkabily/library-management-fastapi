4489dc01dab3041df30995d530d67ff169b83543
KarimAlkabily <karim0alkabily@gmail.com>
2026-04-22 07:24:23 +0200

    first post book and validation

--- app/routes/books.py
+++ app/routes/books.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from app.schemas.book import Book
 
 router=APIRouter()
 books=[]
+
 @router.get("/books")
 def get_books():
     return books
+
+@router.post("/books")
+def create_book(book:Book):
+    books.append(book)
+    return book
