0acdb930a799b01a5c71b29ea8d1569aa89591e0
KarimAlkabily <karim0alkabily@gmail.com>
2026-04-23 21:26:10 +0200

    add user model

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app.routes import books
-from app.models import book
-from app.db import engine
+from app.models import book,user
+from app.db import engine,Base
+
 
 
 app=FastAPI()
 app.include_router(books.router)
 
-book.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
