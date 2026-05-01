07053e8d2c6387cc331f36ea709e55347cbbcde6
karim alkabily <karim0alkabily@gmail.com>
2026-05-01 22:44:05 +0300

    add CrosMiddleWare to main

--- app/main.py
+++ app/main.py
@@ -9,11 +9,21 @@ from app.utils.logger import (
     log_event,
     record_request,
 )
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 
 app=FastAPI()
 app.include_router(books.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware("http")
