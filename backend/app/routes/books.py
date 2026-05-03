458f32c7f8e92a603bcc86ed206e4835f75e2d56
karim alkabily <karim0alkabily@gmail.com>
2026-05-03 23:46:04 +0300

    repair borrow function

--- backend/app/routes/books.py
+++ backend/app/routes/books.py
@@ -1,17 +1,19 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.book import Book
 from app.services import book_service
-from app.schemas.user import UserCreate,UserLogin,UserOut
+from app.schemas.user import UserCreate, UserLogin, UserOut
 from fastapi import Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from app.utils.jwt import get_current_user
-from app.utils.jwt import get_current_admin
+from app.utils.jwt import get_current_user, get_current_admin
+
 security = HTTPBearer()
 router = APIRouter()
+
 
 @router.get("/books")
 def get_books():
     return book_service.get_all_books()
+
 
 @router.get("/books/{book_id}")
 def get_book(book_id: int):
@@ -20,20 +22,22 @@ def get_book(book_id: int):
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
+
 @router.post("/books")
-def create_book(book: Book,user = Depends(get_current_admin)):
+def create_book(book: Book, user=Depends(get_current_admin)):
     return book_service.create_book(book)
 
+
 @router.put("/books/{book_id}")
-def update_book(book_id: int, book: Book,user = Depends(get_current_admin)):
+def update_book(book_id: int, book: Book, user=Depends(get_current_admin)):
     updated = book_service.update_book(book_id, book)
     if not updated:
         raise HTTPException(status_code=404, detail="Book not found")
     return updated
 
-@router.delete("/books/{book_id}")
-def delete_book(book_id: int,user = Depends(get_current_admin)):
 
+@router.delete("/books/{book_id}")
+def delete_book(book_id: int, user=Depends(get_current_admin)):
     deleted = book_service.delete_book(book_id)
     if not deleted:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -46,24 +50,19 @@ def borrow_book(
     credentials: HTTPAuthorizationCredentials = Depends(security)
 ):
     token = credentials.credentials
-
     user = get_current_user(token)
 
     if not user:
         raise HTTPException(status_code=401, detail="Invalid token")
-    
+
     result = book_service.borrow_book(user.id, book_id)
 
-    # error handling
     if result == "user_not_found":
         raise HTTPException(status_code=404, detail="User not found")
-
     if result == "book_not_found":
         raise HTTPException(status_code=404, detail="Book not found")
-
     if result == "book_already_borrowed":
         raise HTTPException(status_code=400, detail="Book already borrowed")
-
     if result == "limit_exceeded":
         raise HTTPException(status_code=400, detail="Borrow limit exceeded")
 
@@ -83,17 +82,15 @@ def return_book(
 
     result = book_service.return_book(user.id, book_id)
 
-    if result=="Record Not Found":
-        raise HTTPException(status_code=404,detail="Borrow record not found")
+    if result == "Record Not Found":
+        raise HTTPException(status_code=404, detail="Borrow record not found")
 
-    return {"message":"Book returnd successfully"}
+    return {"message": "Book returned successfully"}
 
 
-
-#..
 @router.post("/register", response_model=UserOut)
-def register(user:UserCreate):
-    result= book_service.create_user(user)
+def register(user: UserCreate):
+    result = book_service.create_user(user)
 
     if result == "email_exists":
         raise HTTPException(status_code=400, detail="Email already exists")
@@ -106,21 +103,19 @@ def register(user:UserCreate):
     }
 
 
-#..
 @router.post("/login")
-def login(user:UserLogin):
-    result=book_service.login_user(user)
+def login(user: UserLogin):
+    result = book_service.login_user(user)
 
     if result == "invalid_credentials":
         raise HTTPException(status_code=400, detail="Invalid email or password")
 
     return {
-    "access_token": result,
-    "role": user.role
-}
+        "access_token": result["token"],
+        "role": result["role"],
+    }
 
 
-#..history
 @router.get("/history")
 def get_history(
     credentials: HTTPAuthorizationCredentials = Depends(security)
@@ -131,6 +126,4 @@ def get_history(
     if not user:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    history = book_service.get_user_history(user.id)
-
-    return history
+    return book_service.get_user_history(user.id)
--- backend/app/services/book_service.py
+++ backend/app/services/book_service.py
@@ -2,78 +2,67 @@ from app.db import SessionLocal
 from app.models.book import Book
 from app.models.borrow import BorrowRecord
 from app.models.user import User
-from app.schemas.user import UserCreate,UserLogin
-from app.utils.security import hash_password,verify_password
+from app.schemas.user import UserCreate, UserLogin
+from app.utils.security import hash_password, verify_password
 from app.utils.jwt import cerate_access_token
-from datetime import datetime 
+from datetime import datetime
 import json
 from app.db_conn.redis_db import redis_client
 
+
 def get_all_books():
     cached_books = redis_client.get("books")
-
     if cached_books:
         return json.loads(cached_books)
-    
-    db= SessionLocal()
-    books=db.query(Book).all() #redis[object->dict
-    
+
+    db = SessionLocal()
+    books = db.query(Book).all()
     books_data = [{"id": book.id, "title": book.title, "author": book.author} for book in books]
-    
     db.close()
-   
+
     redis_client.set("books", json.dumps(books_data), ex=60)
     return books_data
 
 
 def get_book(book_id):
-    cache_key =f"book:{book_id}"
-    cached_book =redis_client.get(cache_key)
-
+    cache_key = f"book:{book_id}"
+    cached_book = redis_client.get(cache_key)
     if cached_book:
         return json.loads(cached_book)
-    
-    db= SessionLocal()
-    book =db.query(Book).filter(Book.id==book_id).first()
 
+    db = SessionLocal()
+    book = db.query(Book).filter(Book.id == book_id).first()
     if not book:
         db.close()
         return None
-    
-    book_data = {
-        "id": book.id,
-        "title": book.title,
-        "author": book.author
-    }
 
+    book_data = {"id": book.id, "title": book.title, "author": book.author}
     db.close()
 
-    redis_client.set(cache_key,json.dumps(book_data),ex=60)
+    redis_client.set(cache_key, json.dumps(book_data), ex=60)
     return book_data
 
 
 def create_book(book_data):
-    db= SessionLocal()
-    book= Book(title=book_data.title, author= book_data.author)
+    db = SessionLocal()
+    book = Book(title=book_data.title, author=book_data.author)
     db.add(book)
     db.commit()
     redis_client.delete("books")
     db.refresh(book)
     db.close()
     return book
-    
+
 
 def update_book(book_id, book_data):
-    db= SessionLocal()
-    book= db.query(Book).filter(Book.id==book_id).first()
-
+    db = SessionLocal()
+    book = db.query(Book).filter(Book.id == book_id).first()
     if not book:
         db.close()
         return None
-    
-    book.title= book_data.title
-    book.author= book_data.author
 
+    book.title = book_data.title
+    book.author = book_data.author
     db.commit()
     redis_client.delete("books")
     redis_client.delete(f"book:{book_id}")
@@ -81,15 +70,14 @@ def update_book(book_id, book_data):
     db.close()
     return book
 
+
 def delete_book(book_id):
-    db= SessionLocal()
-
-    book= db.query(Book).filter(Book.id==book_id).first()
-
+    db = SessionLocal()
+    book = db.query(Book).filter(Book.id == book_id).first()
     if not book:
         db.close()
         return None
-    
+
     db.delete(book)
     db.commit()
     redis_client.delete("books")
@@ -98,121 +86,102 @@ def delete_book(book_id):
     return book
 
 
+def borrow_book(user_id, book_id):
+    db = SessionLocal()
 
-#...
-def borrow_book(user_id,book_id):
-    db= SessionLocal()
-
-    user=db.query(User).filter(User.id==user_id).first()
+    user = db.query(User).filter(User.id == user_id).first()
     if not user:
         db.close()
         return "user_not_found"
-    
 
-    
-    book=db.query(Book).filter(Book.id==book_id).first()
+    book = db.query(Book).filter(Book.id == book_id).first()
     if not book:
         db.close()
         return "book_not_found"
 
-    borrowed=db.query(BorrowRecord).filter(BorrowRecord.book_id==book_id).first()
+    borrowed = db.query(BorrowRecord).filter(
+    BorrowRecord.book_id == book_id,
+    BorrowRecord.returned == False).first()
+    
     if borrowed:
         db.close()
         return "book_already_borrowed"
-    
 
     user_books_count = db.query(BorrowRecord).filter(BorrowRecord.user_id == user_id).count()
-
-    if user_books_count >=3:
+    if user_books_count >= 3:
         db.close()
         return "limit_exceeded"
 
-
-    borrow= BorrowRecord(user_id=user_id,book_id=book_id)
+    borrow = BorrowRecord(user_id=user_id, book_id=book_id)
     db.add(borrow)
     db.commit()
     db.close()
-
     return "success"
 
 
-
-
-#..
-def return_book(user_id,book_id):
-    db=SessionLocal()
-
-    record=db.query(BorrowRecord).filter(
-        BorrowRecord.user_id==user_id,
-        BorrowRecord.book_id==book_id,
+def return_book(user_id, book_id):
+    db = SessionLocal()
+    record = db.query(BorrowRecord).filter(
+        BorrowRecord.user_id == user_id,
+        BorrowRecord.book_id == book_id,
         BorrowRecord.returned == False
-                                        ).first()
-    
+    ).first()
+
     if not record:
         db.close()
         return "Record Not Found"
-    
+
     record.returned = True
     record.returned_at = datetime.utcnow()
     db.commit()
     db.close()
-
     return "Success"
 
-#..
+
 def create_user(user_data: UserCreate):
     db = SessionLocal()
-
     existing_user = db.query(User).filter(User.email == user_data.email).first()
     if existing_user:
         db.close()
         return "email_exists"
-    
-    hashed_password = hash_password(user_data.password)
 
-    user= User(
+    hashed_password = hash_password(user_data.password)
+    user = User(
         username=user_data.username,
         email=user_data.email,
-        password= hashed_password
+        password=hashed_password
     )
     db.add(user)
     db.commit()
     db.refresh(user)
     db.close()
-
     return user
 
 
-#..
 def login_user(user_data: UserLogin):
     db = SessionLocal()
-
     user = db.query(User).filter(User.email == user_data.email).first()
 
     if not user:
         db.close()
         return "invalid_credentials"
 
-    # verify password
     if not verify_password(user_data.password, user.password):
         db.close()
         return "invalid_credentials"
-    token=cerate_access_token({"user_id":user.id})
+
+    token = cerate_access_token({"user_id": user.id})
+    role = user.role
     db.close()
-    return token
+
+    return {"token": token, "role": role}
 
 
-#..history
 def get_user_history(user_id):
     db = SessionLocal()
-
-    records = db.query(BorrowRecord).filter(
-        BorrowRecord.user_id == user_id
-    ).all()
-
+    records = db.query(BorrowRecord).filter(BorrowRecord.user_id == user_id).all()
     db.close()
     return records
-
 
 
 
